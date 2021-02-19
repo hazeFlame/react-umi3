6e73d4dc51030e9beeac86e3ce72d58ec7c3b7a8
hazeflame <myhazeflame@gmail.com>
2021-02-19 15:04:05 +0800

    ref: 修改自定义table

--- src/components/CustomTable/index.tsx
+++ src/components/CustomTable/index.tsx
@@ -1,43 +1,56 @@
-import React, { FC, ReactNode } from "react"
-import ProTable from "@ant-design/pro-table"
-import { Button } from "antd"
+import React, { FC } from 'react';
+import ProTable from '@ant-design/pro-table';
+import { Button } from 'antd';
 
-import { mapValues, pickBy, trim } from "lodash"
-import { useLocation, history } from "umi"
-// import numeralFormat from "@/utils/numeralFormat"
-import moment from "moment"
-import { INIT_PARAMS } from "@/constants"
-import { getUUID } from "@/utils/utils"
+import { mapValues, pickBy, trim, isEmpty, map } from 'lodash';
+import { useLocation, history } from 'umi';
+import numeralFormat from '@/utils/numeralFormat';
+import moment from 'moment';
+import { INIT_PARAMS } from '@/constants';
+import { getUUID } from '@/utils/utils';
 
-interface ICustomProps { 
-  loading: boolean
-  columns: any[]
-  dataSource: any[]
-  total: number
-  showTotal?: ReactNode
-  searchNode?: any,
-  onRow?: any
-  search?: boolean
-  dateField?: string
-  startDateField?: string
-  endDateField?: string
+/**
+ * @param {String} dateField 时间字段
+ * @param {String} startDateField 时间开始字段
+ * @param {String} endDateField 时间结束字段
+ */
+interface IDateField {
+  dateField: string;
+  startDateField: string;
+  endDateField: string;
 }
 
-const CustomTable: FC<ICustomProps> = ({ 
-  loading, 
-  onRow, 
-  columns, 
-  dataSource, 
-  total, 
-  showTotal, 
-  searchNode, 
+interface ICustomProps {
+  loading: boolean;
+  columns: any[];
+  dataSource: any[];
+  total: number;
+  showTotal?: (r: number) => void;
+  searchNode?: any;
+  onRow?: any;
+  search?: boolean;
+  dateFilters?: IDateField[];
+  exportExcelUrl?: string;
+}
+
+/**
+ *
+ * @param {Boolean} loading
+ * @param {} columns
+ */
+const CustomTable: FC<ICustomProps> = ({
+  loading,
+  columns,
+  dataSource,
+  total,
+  showTotal,
+  searchNode,
+  onRow,
   search = true,
-  dateField,
-  startDateField,
-  endDateField
+  dateFilters,
 }) => {
-  const location: any = useLocation()
-  
+  const location: any = useLocation();
+
   return (
     <ProTable
       loading={loading}
@@ -50,72 +63,90 @@ const CustomTable: FC<ICustomProps> = ({
         total,
         showQuickJumper: true,
         showSizeChanger: true,
-        // showTotal: (t) => showTotal && showTotal(t) || `共有${numeralFormat(t)}条记录`,
+        showTotal: (t) => (showTotal && showTotal(t)) || `共有${numeralFormat(t)}条记录`,
         current: Number(location?.query?.page || INIT_PARAMS.page),
         pageSize: Number(location?.query?.pageSize || INIT_PARAMS.pageSize),
         onChange: (page, pageSize) => {
           history.push({
             pathname: location.pathname,
-            query: { ...location.query, page, pageSize }
-          })
+            query: { ...location.query, page, pageSize },
+          });
         },
         onShowSizeChange: (current, pageSize) => {
           history.push({
             pathname: location.pathname,
-            query: { ...location.query, page: 1, pageSize }
-          })
-        }
+            query: { ...location.query, page: 1, pageSize },
+          });
+        },
       }}
-      search={search ? {
-        labelWidth: 'auto',
-        defaultCollapsed: false,
-        optionRender: (_, { form }) => [
-          searchNode ? searchNode(form) :
-          <Button
-            key="search"
-            type="primary"
-            onClick={() => {
-              let searchQuery = {}
-              if (dateField && startDateField && endDateField) {
-                searchQuery = {
-                  ...form?.getFieldsValue(),
-                  [startDateField]: form?.getFieldValue(dateField) && moment(form?.getFieldValue(dateField)[0]).format('YYYY-MM-DD'),
-                  [endDateField]: form?.getFieldValue(dateField) && moment(form?.getFieldValue(dateField)[1]).format('YYYY-MM-DD'),
-                }
-              } else {
-                searchQuery = form?.getFieldsValue()
-              }
+      search={
+        search
+          ? {
+              labelWidth: 'auto',
+              defaultCollapsed: false,
+              optionRender: (_, { form }) => [
+                searchNode ? (
+                  searchNode(form)
+                ) : (
+                  <Button
+                    key="search"
+                    type="primary"
+                    onClick={() => {
+                      let searchQuery = {};
+                      if (!isEmpty(dateFilters)) {
+                        // 处理时间数据
+                        const fileds = map(
+                          dateFilters?.map((v) => Object.values(v)),
+                          (v) =>
+                            form?.getFieldValue(v[0]) && {
+                              [v[1]]: moment(form?.getFieldValue(v[0])[0]).format('YYYY-MM-DD'),
+                              [v[2]]: moment(form?.getFieldValue(v[0])[1]).format('YYYY-MM-DD'),
+                            },
+                        ).reduce((p, n) => ({ ...p, ...n }), {});
+                        searchQuery = {
+                          ...form?.getFieldsValue(),
+                          ...fileds,
+                        };
+                      } else {
+                        searchQuery = form?.getFieldsValue();
+                      }
 
-              history.push({
-                pathname: location.pathname,
-                query: {
-                  uuid: getUUID(),
-                  ...mapValues(pickBy(searchQuery, (v: any) => !!v), (v: any) => trim(v)),
-                }
-              })
-            }}
-          >
-            查询
-          </Button>,
-          <Button
-            key="rest"
-            onClick={async () => {
-              await history.replace({
-                pathname: location.pathname,
-                query: {
-                  uuid: getUUID()
-                }
-              })
-              setTimeout(() => form?.resetFields(), 0)
-            }}
-          >
-            重置
-          </Button>,
-          <Button key="out">导出</Button>,
-        ],
-      } : false}
+                      history.push({
+                        pathname: location.pathname,
+                        query: {
+                          uuid: getUUID(),
+                          ...mapValues(
+                            pickBy(searchQuery, (v: any) => !!v),
+                            (v: any) => trim(v),
+                          ),
+                        },
+                      });
+                    }}
+                  >
+                    查询
+                  </Button>
+                ),
+                <Button
+                  key="rest"
+                  onClick={async () => {
+                    await history.replace({
+                      pathname: location.pathname,
+                      query: {
+                        uuid: getUUID(),
+                      },
+                    });
+                    setTimeout(() => form?.resetFields(), 0);
+                  }}
+                >
+                  重置
+                </Button>,
+                <Button key="out">导出</Button>,
+              ],
+            }
+          : false
+      }
     />
-  )
-}
+  );
+};
 
-export default CustomTable
+export default CustomTable;
