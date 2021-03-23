c8e76ad2dc592959feae90d919a947c628886b5f
hazeflame <myhazeflame@gmail.com>
2021-03-23 11:19:33 +0800

    ref: update proxy

--- config/proxy.ts
+++ config/proxy.ts
@@ -1,10 +1,6 @@
 export default {
   '/api': {
-    target: 'http://192.168.1.149:19100/',
-    changeOrigin: true,
-  },
-  '/aliHost': {
-    target: 'https://huijie-keji.oss-cn-beijing.aliyuncs.com/', //服务器
+    target: 'http://xxxxx/',
     changeOrigin: true,
   }
 }
